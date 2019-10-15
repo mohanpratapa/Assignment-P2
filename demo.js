a57786f3de8a3a85723f74f33823e44f8e69a9a5
mohanpratapa <pvskrishnamohan24@gmail.com>
2019-10-14 20:50:23 -0500

    local storage added

--- demo.js
+++ demo.js
@@ -11,19 +11,21 @@ function multiplication (x)
 
 
 
-
+localStorage.setItem('number',0);
 document.querySelector('#MyAppcalculate').addEventListener('click',function(){
   console.log("in func")
     const i = parseInt(document.querySelector('#firstNumber').value)
     document.querySelector('#inr').innerHTML = "dollers in rupees "+multiplication(i)
 })
 const updateWithJoke = async (event) => {
+  localStorage.setItem('number',parseInt(localStorage.getItem('number'))+1)
     document.querySelector('#result').innerHTML = ''
     const url = 'https://api.icndb.com/jokes/random?limitTo=[nerdy]'
     const response = await fetch(url)
     const obj = await response.json()
     const joke = obj.value.joke || 'No joke for you.'
     document.querySelector('#result').innerHTML = joke
+    document.querySelector('#local').innerHTML = "cont for number of jokes ="+localStorage.getItem('number')
   }
   document.addEventListener('click', event => {
     if (event.target && event.target.id === 'getJokeButton') { updateWithJoke(event) }
